17a175be0442d4bea5ada592b877c1c63d09dd3c
DonizeteVida <donizetevida1998@outlook.com>
2026-08-10 16:50:37 +0200

    Compile android project

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use jni::objects::JClass;
+use jni::sys::jstring;
+use jni::Env;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[unsafe(no_mangle)]
+pub extern "C" fn Java_com_turing_smart_screen_MainActivity_helloworld(
+    mut env: Env,
+    _class: JClass,
+) -> jstring {
+    env
+    .new_string("Hello World")
+    .expect("Failed to create string")
+    .into_raw()
 }
